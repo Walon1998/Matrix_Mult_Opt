1e3bcdf3e2d7221f27513234de70ec0595efcabb
WaloNeville <neville.walo@gmail.com>
2018-12-03 15:17:34 +0100

    added some shit

--- mmul.c
+++ mmul.c
@@ -2,7 +2,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <x86intrin.h>
+#include <emmintrin.h>
 
+#define SM (CLS / sizeof (int))
 
 
 unsigned long long readTSC() {
@@ -56,6 +58,7 @@ static void blocktiling(int *A, int *B, int *result, int size) {
                 /* block x block mini matrix multiplications */
                 for (int i1 = i; i1 < i + block; i1++) {
                     for (int j1 = j; j1 < j + block; j1++) {
+
                         for (int k1 = k; k1 < k + block; k1++) {
                             result[i1 * size + j1] += A[i1 * size + k1] * B[k1 * size + j1];
                         }
@@ -98,6 +101,7 @@ static void transposesimple(int *A, int *B, int *result, int size) {
             tmp[i * size + j] = B[j * size + i];
         }
     }
+
     for (int i = 0; i < size; ++i) {
         for (int j = 0; j < size; ++j) {
             int sum = 0;
@@ -111,6 +115,32 @@ static void transposesimple(int *A, int *B, int *result, int size) {
 
 }
 
+static void blockvectorize(int *A, int *B, int *result, int size) {
+//    Not working
+
+    int i, i2, j, j2, k, k2;
+    double *restrict rres;
+    double *restrict rmul1;
+    double *restrict rmul2;
+    for (i = 0; i < size; i += SM)
+        for (j = 0; j < size; j += SM)
+            for (k = 0; k < size; k += SM)
+                for (i2 = 0, rres = &result[i * sizeof +j], rmul1 = &A[i * size + k]; i2 < SM;
+                     ++i2, rres += size, rmul1 += size) {
+                    _mm_prefetch (&rmul1[8], _MM_HINT_NTA);
+                    for (k2 = 0, rmul2 = &B[k * size + j]; k2 < SM; ++k2, rmul2 += size) {
+                        __m128d m1d = _mm_load_sd(&rmul1[k2]);
+                        m1d = _mm_unpacklo_pd(m1d, m1d);
+                        for (j2 = 0; j2 < SM; j2 += 2) {
+                            __m128d m2 = _mm_load_pd(&rmul2[j2]);
+                            __m128d r2 = _mm_load_pd(&rres[j2]);
+                            _mm_store_pd(&rres[j2],
+                                         _mm_add_pd(_mm_mul_pd(m2, m1d), r2));
+                        }
+                    }
+                }
+}
+
 
 static int *mmul(int size, int *A, int *B) {
     int *result = malloc(size * size * sizeof(int));
@@ -118,17 +148,17 @@ static int *mmul(int size, int *A, int *B) {
 
     long long t = readTSC();
 
-    transposesimple(A, B, result, size);
+    simple(A, B, result, size);
 
 
-    long long resultcycles = readTSC();
-    long long resultcycles2 = resultcycles - t;
+    double resultcycles = readTSC();
+    double resultcycles2 = resultcycles - t;
 
     if (resultcycles2 < 0) {
         printf("0\n");
     } else {
-        printf("%d\n", resultcycles2);
-        printf("%d\n", resultcycles2);
+        printf("%g\n", resultcycles2);
+//        printf("%d\n", resultcycles2);
     }
 
 
@@ -141,29 +171,29 @@ int main(int argc, char **argv) {
         return 1;
     }
     int size = atoi(argv[1]);
-//    printf("SPCA Simple Matrix Multiplicator. Matrix size: %d\n", size);
+    printf("SPCA Simple Matrix Multiplicator. Matrix size: %d\n", size);
 
-    for (int i = 0; i < size; i += 1) {
-
-
-        int *A = randmatrix(i);
-        //printmatrix(size, A, 'A');
-
-        int *B = randmatrix(i);
-        //printmatrix(size, B, 'B');
+//    for (int i = 0; i < size; i += 1) {
 
 
-        int *C = mmul(i, A, B);
+    int *A = randmatrix(size);
+    //printmatrix(size, A, 'A');
+
+    int *B = randmatrix(size);
+    //printmatrix(size, B, 'B');
 
 
-        // the result needs to be used, otherwise C will not be computed
-        // redirect the output with ./mmul 5000 > output
-        printmatrix(i, C, 'C');
+    int *C = mmul(size, A, B);
 
-        free(A);
-        free(B);
-        free(C);
-    }
+
+    // the result needs to be used, otherwise C will not be computed
+    // redirect the output with ./mmul 5000 > output
+    printmatrix(size, C, 'C');
+
+    free(A);
+    free(B);
+    free(C);
+//    }
 
     return 0;
 }
